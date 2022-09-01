3126062122ba9b89eae414c2678067960466cbff
Nikita Kuznetsov <n.kuznetsov@wanna.fashion>
2022-09-01 18:11:07 +0300

    add constants for wait

--- Example/WannaSDK_ExampleUITests/Extensions/XCUIElement+wait.swift
+++ Example/WannaSDK_ExampleUITests/Extensions/XCUIElement+wait.swift
@@ -5,23 +5,27 @@
 import XCTest
 
 
+struct Constants {
+    static let waitInterval: TimeInterval = 30
+}
+
 extension XCUIElement {
-    
+
     enum WaitPredicate: String {
         case exists = "exists == true"
         case notExists = "exists == false"
     }
     
-    func wait(_ timeout: TimeInterval = 10.0) -> XCUIElement {
+    func wait(_ timeout: TimeInterval = Constants.waitInterval) -> XCUIElement {
         _ = waitForExistence(timeout: timeout)
         return self
     }
     
-    func waitIfNotExists(_ timeout: TimeInterval = 15.0) -> Bool {
+    func waitIfNotExists(_ timeout: TimeInterval = Constants.waitInterval) -> Bool {
         return !waitForExistence(timeout: timeout)
     }
     
-    func waitUntil(predicate: WaitPredicate, timeout: TimeInterval = 30.0) {
+    func waitUntil(predicate: WaitPredicate, timeout: TimeInterval = Constants.waitInterval) {
         let expectation = XCTestCase().expectation(
             for: NSPredicate(format: predicate.rawValue),
             evaluatedWith: self,
