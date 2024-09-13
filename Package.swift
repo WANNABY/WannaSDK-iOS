410fc425e617df649f5df5dacc7db55e3538b547
Denis Kapusta <denis.kapusta@wanna.fashion>
2024-09-13 12:06:32 +0300

    SPM

--- Package.swift
+++ Package.swift
@@ -21,13 +21,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WannaCoreSDK",
-            url: "https://wreleases.s3.amazonaws.com/sdks/ios/20240717/WannaSDK_ios_7_2_0_626_core_spm.zip",
-            checksum: "fa0be316f5659605a3821bf94526c18f4fdc1f187a38fa213d265e9efaef36f0"
+            url: "https://wreleases.s3.amazonaws.com/sdks/ios/20240910/WannaSDK_ios_7_3_0_653_core_spm.zip",
+            checksum: "c25e0de7081676e42ae500fade6c2b8daae546196e6a0b0271253b60b37da6f1"
         ),
         .binaryTarget(
             name: "WsneakersUISDK",
-            url: "https://wreleases.s3.amazonaws.com/sdks/ios/20240717/WannaSDK_ios_7_2_0_626_ui_spm.zip",
-            checksum: "c48a8a9de080b9d4448498a993eaf1a9cdc8cb33a2b97b007a86ac27396abc78"
+            url: "https://wreleases.s3.amazonaws.com/sdks/ios/20240910/WannaSDK_ios_7_3_0_653_ui_spm.zip",
+            checksum: "c8680f148c4e4581c31d03d93940d8bcb09c848ef43639b802264b9f29437d57"
         )
     ]
 )
